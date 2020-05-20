9d1c4016e75553dae14fdf635ef29974e33ce379
loke <1009483976@qq.com>
2020-05-20 11:28:06 +0800

    删除多余文件

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './bx';
+import App from './RN_exam';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
